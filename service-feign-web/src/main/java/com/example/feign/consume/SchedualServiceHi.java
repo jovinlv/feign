19aa46a319d308c821fd0a1a3067590806dd5176
jovinlv <joivn_lv@163.com>
2019-05-14 15:18:26 +0800

    feign消费调用远程接口

--- service-feign-web/src/main/java/com/example/feign/consume/SchedualServiceHi.java
+++ service-feign-web/src/main/java/com/example/feign/consume/SchedualServiceHi.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author Created by jovin .
  * @date Created on 11:17 2019/3/25.
  */
-@FeignClient(value = "service-client",fallback = SchedualServiceHiHystric.class)
+@FeignClient(value = "service-client", fallback = SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
-  @RequestMapping(value = "/hi",method = RequestMethod.GET)
+
+  @RequestMapping(value = "/hi", method = RequestMethod.GET)
   String sayHiFromClientOne(@RequestParam(value = "name") String name);
 }
